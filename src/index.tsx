3ab045ad3c08eb5b1da2058dc4d76e918664926e
Eric Einhaus <eeinhaus17@gmail.com>
2021-08-30 16:41:34 -0400

    JSX transplier form implemented

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,49 @@
-import React from 'react';
+import * as esbuild from 'esbuild-wasm';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState, useEffect, useRef } from 'react';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const esBuildRef = useRef<any>();
+  // User input text
+  const [inputText, setInputText] = useState<string>('');
+  // Output from ESBuild
+  const [code, setCode] = useState<string>('');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // inits esbuild
+  const startService = async () => {
+    esBuildRef.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+
+  const onClick = async () => {
+    if (!esBuildRef.current) return;
+    const result = await esBuildRef.current.transform(inputText, {
+      loader: 'jsx',
+      target: 'es2015',
+    });
+    setCode((code) => (code = result.code));
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  return (
+    <div>
+      <textarea
+        rows={10}
+        cols={20}
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
