37097da87b863708fe2f2d8d2765e06e88a064fa
Eric Einhaus <eeinhaus17@gmail.com>
2021-09-04 20:44:17 -0400

    cleaning up, adding components and folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,91 +1,12 @@
-import * as esbuild from 'esbuild-wasm';
 import ReactDOM from 'react-dom';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import { useState, useEffect, useRef } from 'react';
-import { unpkgPathPlugin } from './plugins/unpgk-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
-import CodeEditor from './components/code-editor';
+import CodeCell from './components/code-cell';
 
 const App = () => {
-  const esBuildRef = useRef<any>();
-  const iFrameRef = useRef<any>();
-
-  // User input text
-  const [inputText, setInputText] = useState<string>('');
-
-  // inits esbuild
-  const startService = async () => {
-    esBuildRef.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    });
-  };
-
-  const onClick = async () => {
-    if (!esBuildRef.current) return;
-
-    iFrameRef.current.srcdoc = html;
-
-    const result = await esBuildRef.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(inputText)],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window',
-      },
-    });
-
-    iFrameRef.current.contentWindow.postMessage(
-      result.outputFiles[0].text,
-      '*'
-    );
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
-
-  const html = `
-  <html>
-    <head></head>
-    <body>
-      <div id='root'></div>
-      <script>
-        window.addEventListener(
-        'message',
-        (e) => {
-          try {
-            eval(e.data)
-          } catch (err) {
-            const root = document.querySelector('#root');
-            root.innerHTML = \`<div style="color: red;"><h4>Runtime Error</h4>\${err}</div>\`;
-            console.error(err);
-          }
-        },
-        false
-        );
-      </script>
-    </body>
-  </html>
-  `;
-
   return (
     <div>
-      <CodeEditor
-        initialValue='const a = 1;'
-        onChange={(value) => setInputText(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <iframe
-        title='codeIFrame'
-        srcDoc={html}
-        sandbox='allow-scripts'
-        ref={iFrameRef}
-      ></iframe>
+      <CodeCell />
+      <CodeCell />
     </div>
   );
 };
