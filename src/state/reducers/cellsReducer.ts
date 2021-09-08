d10250f1234a8e0cc3bc3642c7de42d7b9e01aa3
Eric Einhaus <eeinhaus17@gmail.com>
2021-09-08 09:34:15 -0400

    Successfully selected items from redux store, can display components

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,3 +1,4 @@
+import produce from 'immer';
 import { ActionType } from '../action-types';
 import { Action } from '../actions';
 import { Cell } from '../cell';
@@ -18,22 +19,67 @@ const initialState: CellsState = {
   data: {},
 };
 
-const cellsReducer = (
-  state: CellsState = initialState,
-  action: Action
-): CellsState => {
-  switch (action.type) {
-    case ActionType.MOVE_CELL:
-      return state;
-    case ActionType.DELETE_CELL:
-      return state;
-    case ActionType.INSERT_CELL_BEFORE:
-      return state;
-    case ActionType.UPDATE_CELL:
-      return state;
-    default:
-      return state;
-  }
+const cellsReducer = produce(
+  (state: CellsState = initialState, action: Action): CellsState => {
+    switch (action.type) {
+      case ActionType.MOVE_CELL:
+        const { id: payloadId, direction } = action.payload;
+        const payloadIndex = state.order.findIndex((id) => id === payloadId);
+        const targetIndex =
+          direction === 'up' ? payloadIndex - 1 : payloadIndex + 1;
+
+        if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+          return state;
+        }
+
+        state.order[payloadIndex] = state.order[targetIndex];
+        state.order[targetIndex] = payloadId;
+        return state;
+      case ActionType.DELETE_CELL:
+        delete state.data[action.payload];
+        state.order = state.order.filter((id) => id !== action.payload);
+        return state;
+      case ActionType.INSERT_CELL_BEFORE:
+        const cell: Cell = {
+          content: '',
+          type: action.payload.type,
+          id: randomId(),
+        };
+        state.data[cell.id] = cell;
+
+        const foundIndex = state.order.findIndex(
+          (id) => id === action.payload.id
+        );
+        if (foundIndex < 0) {
+          state.order.push(cell.id);
+        } else {
+          state.order.splice(foundIndex, 0, cell.id);
+        }
+        return state;
+      case ActionType.UPDATE_CELL:
+        const { id, content } = action.payload;
+        /* return {
+          ...state,
+          data: {
+            ...state.data,
+            [id]: {
+              ...state.data[id],
+              content,
+            },
+          },
+        }; */
+        state.data[id].content = content;
+        return state;
+      default:
+        return state;
+    }
+  },
+  initialState
+);
+
+const randomId = () => {
+  //36 - generates 0-9 & a-z
+  return Math.random().toString(36).substring(2, 10);
 };
 
 export default cellsReducer;
