bda0e0d8bbc2205c606cb79b757584e7a4ae1914
Eric Einhaus <eeinhaus17@gmail.com>
2021-09-09 09:12:21 -0400

    Add cell styling

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,5 +1,7 @@
 import CellListItem from './cell-list-item';
 import { useTypedSelector } from '../hooks/use-typed-selector';
+import AddCell from './add-cell';
+import { Fragment } from 'react';
 
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) =>
@@ -7,10 +9,18 @@ const CellList: React.FC = () => {
   );
 
   const renderedCells = cells.map((cell) => (
-    <CellListItem key={cell.id} cell={cell} />
+    <Fragment key={cell.id}>
+      <AddCell nextCellId={cell.id} />
+      <CellListItem key={cell.id} cell={cell} />
+    </Fragment>
   ));
 
-  return <div>{renderedCells}</div>;
+  return (
+    <div>
+      {renderedCells}
+      <AddCell forceVisible={cells.length === 0} nextCellId={null} />
+    </div>
+  );
 };
 
 export default CellList;
